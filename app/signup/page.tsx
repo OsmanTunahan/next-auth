c02578dee4ece78201183e84bba74be4dc120b1a
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-21 19:32:20 +0300

    update signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -65,14 +65,14 @@ export default function Login() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Sign up to your account
               </h1>
               <div className="text-left">
                 <label
                   htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your Username
+                  Username
                 </label>
                 <input
                   onChange={(e) =>
@@ -90,7 +90,7 @@ export default function Login() {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  Email
                 </label>
                 <input
                   onChange={(e) => setUser({ ...user, email: e.target.value })}
