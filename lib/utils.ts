4595c99b624ce1218ceb348c3330c28d3dacdada
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-21 17:48:50 +0300

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,12 +2,12 @@ import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
 const getDataFromToken = (req: NextRequest) => {
-    try {
-        const token = req.cookies.get("access_token")?.value ?? "";
-        return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
-    } catch (error: any) {
-        throw new Error(error.message);
-    }
+  try {
+    const token = req.cookies.get("access_token")?.value ?? "";
+    return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
 
 export { getDataFromToken };
