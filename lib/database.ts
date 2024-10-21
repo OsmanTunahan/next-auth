2d88fdae737a42234f1150ec9b43e82551327c7f
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-21 17:16:16 +0300

    add database connection

---
+++ lib/database.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connect = async () => {
+    //check if the connection is already open
+    if (isConnected) {
+        console.log("using existing database connection");
+        return;
+    }
+    
+    console.log("using new database connection");
+    const db = await mongoose.connect(process.env.MONGODB_URI!, {
+        dbName: process.env.MONGODB_NAME
+    });
+
+    //check if the connection is successful
+    isConnected = db.connections[0].readyState == 1;
+}
+
+export default connect;
