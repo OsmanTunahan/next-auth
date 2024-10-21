7b46a7f7ea1ef84c2f6f5b9057b6be32a76df6ac
OsmanTunahan <osmantunahan007@gmail.com>
2024-10-21 19:12:44 +0300

    add login page

---
+++ app/login/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "@/assets/globals.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const metadata: Metadata = {
+  title: "Login Page",
+  description: "Login page for the app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <ToastContainer />
+        {children}
+      </body>
+    </html>
+  );
+}
